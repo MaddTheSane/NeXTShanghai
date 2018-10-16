e6840302b361e8c14856fd42c918b605f4d37024
C.W. Betts <computers57@hotmail.com>
2018-10-16 00:18:17 -0600

    Initial Commit

---
+++ NeXTShanghai/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NeXTShanghai
+//
+//  Created by C.W. Betts on 10/16/18.
+//  Copyright © 2018 C.W. Betts. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
