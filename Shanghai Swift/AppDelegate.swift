d4e765fe87110e0620a41139ee779ae4d0df809c
C.W. Betts <computers57@hotmail.com>
2018-10-17 02:35:56 -0600

    Begin porting the Shanghai code to Swift.

---
+++ Shanghai Swift/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Shanghai Swift
+//
+//  Created by C.W. Betts on 10/17/18.
+//  Copyright © 2018 C.W. Betts. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
